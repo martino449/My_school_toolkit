c66af8c2986dae9cad3eceb426056dcd11b9633e
Martin449 <76888581+martino449@users.noreply.github.com>
2024-09-10 13:54:50 +0200

    Update M_func_toolkit.py

--- apps/M_func_toolkit.py
+++ apps/M_func_toolkit.py
@@ -4,7 +4,7 @@ import ctypes
 import shutil
 import subprocess
 import sys
-version = "0.1.5"
+version = "0.2.1"
 
 def M_info() -> None:
     print(f"M_func_toolkit \nAuthor: Mario Pisano \n{version} \nLicense: EUPL 1.2 \nCopyright: 2024 Mario Pisano")
